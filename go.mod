e3f78a5944242017be199a9f231d69855bebc9c2
kaisawind <wind.kaisa@gmail.com>
2020-01-05 18:31:02 +0800

    add server

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/kaisawind/protocol
+
+go 1.13
+
+require (
+	github.com/sirupsen/logrus v1.4.2
+	github.com/vugu/vugu v0.1.0
+)
---
+++ server.go
@@ -0,0 +1,42 @@
+// +build !wasm
+
+package main
+
+//go:generate vugugen .
+
+import (
+	"flag"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+	"github.com/vugu/vugu/simplehttp"
+)
+
+func main() {
+	dev := flag.Bool("dev", false, "Enable development features")
+	dir := flag.String("dir", ".", "Project directory")
+	host := flag.String("host", "0.0.0.0", "Listen for HTTP on this host")
+	port := flag.String("port", "8877", "Listen for HTTP on this port")
+	flag.Parse()
+	wd, err := filepath.Abs(*dir)
+	if err != nil {
+		logrus.WithError(err).Errorln("join dir error")
+		return
+	}
+	err = os.Chdir(wd)
+	if err != nil {
+		logrus.WithError(err).Errorln("change work dir error")
+		return
+	}
+	address := net.JoinHostPort(*host, *port)
+	_, err = net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		logrus.WithError(err).Errorln("resolve tcp addr error")
+	}
+	logrus.Infoln("Starting HTTP Server at ", address)
+	h := simplehttp.New(wd, *dev)
+	logrus.Fatal(http.ListenAndServe(*host, h))
+}
